de815ddb3174bcda1d30bbd68ae49424858ff9b8
zainsoftyemen <zain.soft.yemen@gmail.com>
2025-09-07 01:43:12 +0300

    last update for app 1

--- sw.js
+++ sw.js
@@ -1,12 +1,13 @@
-
-const CACHE_NAME = 'hizoum-crm-cache-v1';
+const CACHE_NAME = 'hizoum-crm-cache-v2';
 const urlsToCache = [
   '/',
   '/index.html',
   '/style.css',
   '/script.js',
   '/customers.html',
-  '/icon.png'
+  '/icon.png',
+  '/icon-512.png',
+  '/manifest.json'
 ];
 
 self.addEventListener('install', event => {
@@ -19,14 +20,31 @@ self.addEventListener('install', event => {
   );
 });
 
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.match(event.request).then(response => {
+        const fetchPromise = fetch(event.request).then(networkResponse => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+        return response || fetchPromise;
+      });
+    })
   );
 });
